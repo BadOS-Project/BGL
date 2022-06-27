b9179f68c109230ef7cfbb32cb3f2dc90a812629
pointer-to-bios <yyb1738487169@163.com>
2022-06-27 16:44:03 +0800

    init

--- includes/BGL.hh
+++ includes/BGL.hh
@@ -1,6 +1,20 @@
 #ifndef __BGL_hh_
 #define __BGL_hh_
 
+/**
+ * @file BGL.hh
+ * @author pointer-to-bios
+ * @brief Bad Graphics Library
+ * @version 0.1
+ * @date 2022-06-27
+ * 
+ * @copyright Copyright (c) 2022 Random World Studio
+ * 
+ */
+#include "BGL/window.hh"
 
+namespace bgl
+{
+};
 
 #endif
