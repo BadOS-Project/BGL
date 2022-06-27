b2543f685d586a4ca1b5e6562b5454739f5ab93e
pointer-to-bios <yyb1738487169@163.com>
2022-06-27 16:53:11 +0800

    add class Point and Line

--- includes/BGL/base/shape.hh
+++ includes/BGL/base/shape.hh
@@ -9,10 +9,21 @@ namespace bgl
     {
     public:
         int64_t x, y;
+
         Point();
         Point(int64_t x, int64_t y);
         void setX(int64_t x);
         void setY(int64_t y);
+    };
+
+    class Line
+    {
+    public:
+        Point p1, p2;
+
+        Line();
+        Line(Point p1, Point p2);
+        Line(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
     };
 
 };
--- libs/base/shape.cc
+++ libs/base/shape.cc
@@ -22,4 +22,20 @@ namespace bgl
         this->y = y;
     }
 
+    Line::Line()
+    {
+    }
+
+    Line::Line(Point p1, Point p2)
+    {
+        this->p1 = p1;
+        this->p2 = p2;
+    }
+
+    Line::Line(int64_t x1, int64_t y1, int64_t x2, int64_t y2)
+    {
+        this->p1 = Point(x1, y1);
+        this->p2 = Point(x2, y2);
+    }
+
 };
