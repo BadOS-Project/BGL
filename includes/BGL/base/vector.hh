a9730951491ca960d945a2912f7e36e919b9c8f2
pointer-to-bios <yyb1738487169@163.com>
2022-07-03 22:30:00 +0800

    add Vecot2 class: 2D vector

--- includes/BGL/base/vector.hh
+++ includes/BGL/base/vector.hh
@@ -1,9 +1,23 @@
 #ifndef __vector_hh_
 #define __vector_hh_
 
+#include <bits/stdint-intn.h>
+
 namespace bgl
 {
+    class Vector2
+    {
+    public:
+        int64_t x, y;
+        Vector2(int64_t x, int64_t y);
 
+        Vector2 operator+(Vector2 v);
+        Vector2 operator-(Vector2 v);
+        double operator*(Vector2 v);//点积
+        double operator^(Vector2 v);//叉积
+        double operator~();//模长
+    };
+    
 };
 
 #endif
--- libs/base/vector.cc
+++ libs/base/vector.cc
@@ -1,1 +1,39 @@
 #include "../../includes/BGL/base/vector.hh"
+
+#include <cmath>
+
+namespace bgl
+{
+    Vector2::Vector2(int64_t x, int64_t y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    Vector2 Vector2::operator+(Vector2 v)
+    {
+        return Vector2(this->x + v.x, this->y + v.y);
+    }
+
+    Vector2 Vector2::operator-(Vector2 v)
+    {
+        return Vector2(this->x - v.x, this->y - v.y);
+    }
+
+    double Vector2::operator~()
+    {
+        return sqrt(x * x + y * y);
+    }
+
+    double Vector2::operator*(Vector2 v)
+    {
+        return (double)this->x * v.x + this->y * v.y;
+    }
+
+    double Vector2::operator^(Vector2 v)
+    {
+        double costheta = *this * v / (~*this) / ~v;
+        return ~*this * ~v * sqrt(1 - costheta * costheta);
+    }
+
+};
